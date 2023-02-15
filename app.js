f54830f97b6ebf20c53e4f0dcb0bf73255cf1eed
laoluwapo <alividegabriel9@gmail.com>
2023-02-15 19:56:12 +0100

    Add the sounds to the sound loop.

--- app.js
+++ app.js
@@ -6,26 +6,57 @@ class DrumKit {
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.bpm = 40;
+    this.bpm = 150;
   }
   // Method that loops over all the pads
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+    // Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // check if pads are active
+      if (bar.classList.contains("active")) {
+        // check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
-  // A method that controls how fast the pads are looped over
-  // depending on the bpm set.
+  // A method that controls how fast the pads are looped over depending on the bpm set.
   start() {
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
     }, interval);
   }
+  // A method that adds the active class to the pad on click
+  activePad() {
+    this.classList.toggle("active");
+  }
 }
 
+// Create an instance of an object
 const drumKit = new DrumKit();
+
+// Loop over the pads and run activePad method for any that is clicked
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
 
 drumKit.playButton.addEventListener("click", () => {
   drumKit.start();
